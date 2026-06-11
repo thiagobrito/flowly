96cb5ec56a1ee074eb9c85beaedada0c671d94c7
Thiago Brito <contato@thiagobrito.com.br>
2026-06-10 22:20:56 -0300

    adicionando sentry no aplicativo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,25 @@
 import '../global.css';
 
+import * as Sentry from '@sentry/react-native';
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function Layout() {
+Sentry.init({
+  dsn: 'https://a2476257c2a1c6711c1f36d720aa3342@o1297145.ingest.us.sentry.io/4511544158715904',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
@@ -12,4 +27,4 @@ export default function Layout() {
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
-}
+});
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
-const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
 config.resolver.sourceExts.push('mjs');
 
